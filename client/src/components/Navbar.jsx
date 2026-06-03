a08e3860a1c69a599c793139a442f1f3989bd954
JithendraBasireddy <jithendrareddybasireddy29@gmail.com>
2026-06-03 11:48:37 +0530

    feat: change cart badge to show product count instead of quantity

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -54,7 +54,7 @@ export default function Navbar() {
                 <span className="text-xl">🛒</span>
                 {cart.length > 0 && (
                   <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-indigo-600 rounded-full">
-                    {cart.reduce((sum, item) => sum + item.quantityOrdered, 0)}
+                    {cart.length}
                   </span>
                 )}
               </Link>
